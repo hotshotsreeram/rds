06c37f0707434fadd8cf7c60ef015ca2f9d18d18
hotshotsreeram <hotshotsreeram@gmail.com>
2022-09-12 19:50:49 -0400

    first commit

---
+++ main.tf
@@ -0,0 +1,56 @@
+terraform {
+  required_version = ">=0.12"
+}
+
+provider "aws" {
+  region  = var.aws_region
+}
+
+resource "aws_vpc" "db_vpc" {
+  cidr_block = var.custom_vpc
+
+  tags = {
+    Name = var.vpc_tags
+  }
+}
+
+resource "aws_subnet" "db_private_subnet" {
+  vpc_id            = aws_vpc.db_vpc.id
+  cidr_block        = var.private_subnet1
+  availability_zone = var.aws_zone1
+
+  tags = {
+    Name = var.private_subnet_tags1
+  }
+}
+
+resource "aws_subnet" "db_private_subnet2" {
+  vpc_id            = aws_vpc.db_vpc.id
+  cidr_block        = var.private_subnet2
+  availability_zone = var.aws_zone2
+
+  tags = {
+    Name = var.private_subnet_tags2
+  }
+}
+
+resource "aws_db_subnet_group" "db-subnet" {
+name = "DB subnet group"
+subnet_ids = [aws_subnet.db_private_subnet1.id, aws_subnet.db_private_subnet2.id]
+
+tags = {
+  Name = var.subnet-group-name
+}
+
+resource "aws_db_instance" "practice_instance" {
+  allocated_storage    = 10
+  engine               = var.engine
+  engine_version       = var.engine_version
+  instance_class       = var.instance_class
+  name                 = var.name
+  username             = var.username
+  password             = var.password
+  parameter_group_name = var.parameter_group_name
+  skip_final_snapshot  = true
+  db_subnet_group_name = aws_db_subnet_group.db-subnet.name
+}
---
+++ variables.tf
@@ -0,0 +1,84 @@
+variable "aws_zone1"{
+  type = string
+  description = "available aws zones"
+}
+
+variable "aws_zone2"{
+  type = string
+  description = "available aws zones"
+}
+
+variable "vpc_tags"{
+	type= string
+	description= "tags for vpc"
+}
+
+variable "private_subnet_tags1"{
+	type= string
+	description= "tags for private subnet"
+}
+
+variable "private_subnet_tags2"{
+	type= string
+	description= "tags for private subnet"
+}
+
+variable "private_subnet1" {
+  type        = string
+  description = "private subnet ip cidr block"
+}
+
+variable "private_subnet2" {
+  type        = string
+  description = "private subnet ip cidr block"
+}
+
+variable "custom_vpc" {
+  type        = string
+  description = "custom vpc ip cidr block"
+}
+
+variable "aws_region" {
+  type        = string
+  description = "aws region for the instance and lb"
+}
+
+variable "engine"{
+  type = string
+  desctiption = "engine name"
+}
+
+variable "engine_version"{
+  type = string
+  description = "version of the engine"
+}
+
+variable "instance_class"{
+  type = string
+  description = "instance class"
+}
+
+variable "name"{
+  type = string
+  description = "db instance name"
+}
+
+variable "username"{
+  type = string
+  description = "db instance username"
+}
+
+variable "password"{
+  type= string
+  description = "db instance password"
+}
+
+variable "parameter_group_name"{
+  type = string
+  description = "parameter group name"
+}
+
+variabl "subnet-group-name"{
+  type = string
+  description = "name of the subnet group"
+}
