3084f79ce368f88f41db945b8633754a5b42aab1
hotshotsreeram <hotshotsreeram@gmail.com>
2022-09-12 20:38:10 -0400

    9th commit

--- main.tf
+++ main.tf
@@ -49,7 +49,7 @@ resource "aws_db_instance" "practice_instance" {
   engine               = var.engine
   engine_version       = var.engine_version
   instance_class       = var.instance_class
-  name                 = var.name
+  db_name                 = var.name
   username             = var.username
   password             = var.password
   parameter_group_name = var.parameter_group_name
